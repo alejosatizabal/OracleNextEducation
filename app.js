651a823da3becadbe8b983c51b6edce43c820b16
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-27 00:45:55 -0500

    Avanzando en Taller 2

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
 
-let max = 100;
+let max = 10;
+let numeroSecreto = generarNumeroSecreto()
 
-let parrafo = document.querySelector('p');
-//parrafo.innerHTML = 'Indica un número del 1 al 10';
-parrafo.innerHTML = `Indica un número del 1 al ${max}`;
+console.log(numeroSecreto)
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento () {
+    //let numeroDeUsuario = document.querySelector('input')
+    let numeroDeUsuario = document.getElementById('valorUsuario').value
+    console.log(numeroDeUsuario)
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*max)+1;
+}
+
+asignarTextoElemento('h1', '¡Juego del número secreto!')
+asignarTextoElemento('p', `Indica un número del 1 al ${max}`)
