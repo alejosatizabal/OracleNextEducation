0879c5b83e05899b572d890ed736aea73ed8d2b2
alejosatizabal <alejosatizabal@yahoo.es>
2024-03-08 17:05:05 -0500

    Haciendo Challenge

--- Principiante programación/Challenge0/script.js
+++ Principiante programación/Challenge0/script.js
@@ -43,3 +43,20 @@ function desencriptar(stringDesencriptada) {
     }
     return stringDesencriptada
 }
+
+function copiarTexto() {
+    // Get the text field
+    //var copyText = document.getElementById("myInput");
+    var copyText = document.querySelector('.mensaje');
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    //alert("Texto copiado: " + copyText.value);
+    mensaje.value = "El texto fue copiado"
+  } 
